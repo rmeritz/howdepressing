c6c8b81fce3860918a45cde70c26fac081a7cddd
Rebecca Meritz <rjmeritz@gmail.com>
2012-11-26 22:31:09 +0100

    Remove capture

--- sunlight/urls.py
+++ sunlight/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from sunlight import views
 
 urlpatterns = patterns('',
-                       url(r'(.)+$', views.location, name='location'),
+                       url(r'.+$', views.location, name='location'),
                        url(r'', views.index, name='index')
 )
