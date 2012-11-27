920414457809d5179ca1cc900d64ad618b5f1114
Rebecca Meritz <rjmeritz@gmail.com>
2012-11-27 23:31:37 +0100

    start down an implemntation of the sunrise getter

--- sunlight/tests.py
+++ sunlight/tests.py
@@ -1,9 +1,8 @@
-from django.test import LiveServerTestCase
+from django.test import LiveServerTestCase, unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class AcceptanceTest(LiveServerTestCase):
-
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -19,3 +18,40 @@ class AcceptanceTest(LiveServerTestCase):
 
         body = self.browser.find_element_by_tag_name('body')
         assert "Very Depressing" == body.get_attribute("innerHTML")
+
+class FakeLocation:
+    def city(self):
+        return 'Boston'
+    def state(self):
+        return 'Georgia'
+    def country(self):
+        return 'USA'
+
+class FakeAstronomy:
+    def __init__(self, sunrise, sunset):
+        self.sunrise = sunrise
+        self.sunset = sunset
+
+    def sunset(self):
+        return self.sunset
+    def sunrise(self):
+        return self.sunrise
+
+class FakeWeatherApi:
+    def __init__(self, sunrise = 800, sunset = 1900, full_location = FakeLocation()):
+        self.sunrise = sunrise
+        self.sunset = sunset
+        self.full_location = full_location
+
+    def full_location(self, input_string):
+        return self.full_location
+
+    def astronomy(self):
+        return FakeAstronomy(sunrise = self.sunrise, sunset = self.sunset)
+
+class SunlightDetectorTest(unittest.TestCase):
+    def testTextForAVeryDepressingPlace(self):
+        fake_weather_api = FakeWeatherApi(sunrise = 1000, sunset = 1400)
+        detector = SunlightDetector('stockholm', fake_weather_api)
+        depression_indicator = decector.detect
+        assert "Very Depressing" == depression_indicator.text
